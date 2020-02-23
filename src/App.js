19ad25487b625d6b545e306694020dc1de48b0a9
Gui <guilhermehenry60@gmail.com>
2020-02-23 19:10:29 -0300

    add config reduce

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import store from './store';
 import GlobalStyles from './styles/global';
 import Header from './Components/Header';
 import Routes from './routes';
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyles />
-      <Header />
-      <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyles />
+        <Header />
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
